400ae79d4af1b2820adc059bea65a10204127662
DaNStanK <daniel.stankovski@gmail.com>
2024-11-28 09:39:24 +0100

    Modified the .env login

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,14 @@
-require('dotenv').config;
 const { defineConfig } = require("cypress");
+const dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-    env: {
-      LOGIN_USERNAME: process.env.MODELIKS_USERNAME,
-      LOGIN_PASSWORD: process.env.MODELIKS_PASSWORD
+      config.env.LOGIN_USERNAME = process.env.MODELIKS_USERNAME;
+      config.env.LOGIN_PASSWORD = process.env.MODELIKS_PASSWORD;
+      return config;
     },
     baseUrl: "https://test.hz.modeliks.com"
   },
