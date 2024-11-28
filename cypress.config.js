299646c0c8043bdcce59ba05f11cc36b60411c42
DaNStanK <daniel.stankovski@gmail.com>
2024-11-29 00:06:22 +0100

    Viewport width and height change

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,16 @@
+require('dotenv').config();
 const { defineConfig } = require("cypress");
-const dotenv = require('dotenv');
-dotenv.config();
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Load environment variables
       config.env.LOGIN_USERNAME = process.env.MODELIKS_USERNAME;
       config.env.LOGIN_PASSWORD = process.env.MODELIKS_PASSWORD;
       return config;
     },
-    viewportWidth: 1920, // Set the default width of the viewport
-    viewportHeight: 1080, // Set the default height of the viewport
+    viewportWidth: 1440, // Set the default width of the viewport
+    viewportHeight: 900, // Set the default height of the viewport
     baseUrl: "https://test.hz.modeliks.com"
   },
 });
