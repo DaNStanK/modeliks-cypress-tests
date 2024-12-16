00830c62e0595a2f401ef0bedfd330ea13ae69a7
DaNStanK <daniel.stankovski@gmail.com>
2024-12-16 15:11:30 +0100

    Revenue only test finished

--- cypress/e2e/forecast-revenues.cy.js
+++ cypress/e2e/forecast-revenues.cy.js
@@ -16,17 +16,17 @@ describe('Forecast / Revenues Module', () => {
     // Login the user
     cy.loginUser(Cypress.env('LOGIN_USERNAME'), Cypress.env('LOGIN_PASSWORD'));
 
-    // cy.intercept('GET', `/api/organizational-structure?CompanyID=${company.number}`).as('oranizationalStructure');
-    // cy.intercept('GET', `/api/chart_of_accounts?CompanyID=${company.number}`).as('chartOfAccounts');
+    cy.intercept('GET', `/api/organizational-structure?CompanyID=${company.number}`).as('oranizationalStructure');
+    cy.intercept('GET', `/api/chart_of_accounts?CompanyID=${company.number}`).as('chartOfAccounts');
 
-    // // Wait for all fetches to complete
-    // cy.wait('@oranizationalStructure', { timeout: 100000 })
-    //   .its('response.statusCode')
-    //   .should('eq', 200);
+    // Wait for all fetches to complete
+    cy.wait('@oranizationalStructure', { timeout: 100000 })
+      .its('response.statusCode')
+      .should('eq', 200);
 
-    // cy.wait('@chartOfAccounts', { timeout: 100000 })
-    //   .its('response.statusCode')
-    //   .should('eq', 200);
+    cy.wait('@chartOfAccounts', { timeout: 100000 })
+      .its('response.statusCode')
+      .should('eq', 200);
   });
 
   it.only('Should be able to create product sales revenues', () => {
@@ -178,7 +178,7 @@ describe('Forecast / Revenues Module', () => {
 
 
     // Click the set button for the subunits of business unit 1 from the org. structure
-    cy.setTotals(company.organizationalStructure.levelTwo.name);
+    cy.setTotals(company.organizationalStructure.levelTwo_unitOne.name);
 
     // Set subunit 1 of business unit 1 allocation for 1st month
     cy.editAllocationTableCell(1, 1, product_sales.bu1_subunit1); // row, month, value
@@ -235,7 +235,7 @@ describe('Forecast / Revenues Module', () => {
 
 
     // Click the set button for the subunits of business unit 2 from the org. structure
-    cy.setTotals(company.organizationalStructure.levelThree.name);
+    cy.setTotals(company.organizationalStructure.levelTwo_unitTwo.name);
 
     // Set subunit 1 of business unit 2 allocation for 1st month
     cy.editAllocationTableCell(1, 1, product_sales.bu2_subunit1); // row, month, value
@@ -252,8 +252,8 @@ describe('Forecast / Revenues Module', () => {
     //  *************************************************  \\
 
 
-    // Click the set button for the subunits of business unit 2 from the org. structure
-    cy.setTotals(company.organizationalStructure.levelThree.name);
+    // // Click the set button for the subunits of business unit 2 from the org. structure
+    // cy.setTotals(company.organizationalStructure.levelThree.name);
 
     // Set subunit 1 of business unit 2 allocation for 1st month
     cy.editAllocationTableCell(2, 1, product_sales.bu2_subunit2); // row, month, value
@@ -265,7 +265,7 @@ describe('Forecast / Revenues Module', () => {
     cy.applyToAllFieldsAllocation(2, 1); // row, month
 
     // Click the set button for the subunits of business unit 2 from the org. structure
-    cy.setTotals(company.organizationalStructure.levelTwo.name);
+    cy.setTotals(company.organizationalStructure.levelOne.name);
 
 
     //  *************************************************  \\
@@ -323,6 +323,165 @@ describe('Forecast / Revenues Module', () => {
     // // Click the confirm button on delete option
     // cy.clickButton('Confirm');
   });
+
+  it.only('Should be able to create revenue only', () => {
+    // Assert if you are on Forecast revenues section
+    cy.expectedUrl('https://test.hz.modeliks.com/forecast/revenue');
+
+    // Click on Add Revenue Stream
+    cy.clickButton('Add Revenue Stream');
+
+    // Populate Revenue Name input field
+    cy.setRevenueName(revenue_only.name);
+
+    // Choose the type of revenue
+    cy.chooseRevenueType(revenue_only);
+
+    // Assert that the actual checkbox is checked after clicking the custom UI
+    cy.assertRevenueType(revenue_only.index);
+
+    // Select advance settings
+    cy.chooseAdvanceSettings();
+
+    // Choose planning level
+    cy.choosePlanningLevel('BU');
+
+    // Set the allocation methodology
+    cy.setAllocationMethodology('breakdown');
+
+    // Click save button in the advanced settings
+    cy.clickButton('Save');
+
+    // Click next button and continue to unit sales info section
+    cy.clickButton('Next');
+
+
+    //  *************************************************  \\
+    //                    REVENUE SETUP                    \\
+    //  *************************************************  \\
+
+
+    // Set Unit Sales info for the 1st month
+    cy.editTableCell(1, 1, revenue_only.unit1_revenue_lvl2); // row, month, value
+
+    // Check if the value for the 1st month is correctly applied
+    cy.checkCellValue(1, 1, revenue_only.unit1_revenue_lvl2); // row, month, value
+
+    // Apply the units set for the 1st month to all remaining
+    cy.applyToAllFields(1, 1); // row, month
+
+    // Set Unit Sales info for the 12th month
+    cy.editTableCell(2, 1, revenue_only.unit2_revenue_lvl2); // row, month, value
+
+    // Check if the value is correctly applied on 12th month
+    cy.checkCellValue(2, 1, revenue_only.unit2_revenue_lvl2); // row, month, value
+
+    // Apply the units set for the 1st month to all remaining
+    cy.applyToAllFields(2, 1); // row, month
+
+    // Click next button and continue to unit prices setup
+    cy.clickButton('Next');
+
+
+    //  *************************************************  \\
+    //    ALLOCATION SETUP FOR SUBUNIT 1 AND 2 OF UNIT 1   \\
+    //  *************************************************  \\
+
+
+    // Click the set button for the subunits of business unit 1 from the org. structure
+    cy.setTotals(company.organizationalStructure.levelTwo_unitOne.name);
+
+    // Set subunit 1 of business unit 1 allocation for 1st month
+    cy.editAllocationTableCell(1, 1, revenue_only.unit1_subunit1_revenue_lvl2); // row, month, value
+
+    // Check if the value was set for the 1st month
+    cy.checkAllocationCellValue(1, 1, revenue_only.unit1_subunit1_revenue_lvl2); // row, month, value
+
+    // Apply to all remaining months
+    cy.applyToAllFieldsAllocation(1, 1); // row, month
+
+    // Set subunit 1 of business unit 1 allocation for 12th month
+    cy.editAllocationTableCell(2, 1, revenue_only.unit1_subunit2_revenue_lvl2); // row, month, value
+
+    // Check if the value was set for the 12th month
+    cy.checkAllocationCellValue(2, 1, revenue_only.unit1_subunit2_revenue_lvl2); // row, month, value
+
+    // Apply to all remaining months
+    cy.applyToAllFieldsAllocation(2, 1); // row, month
+
+    //  *************************************************  \\
+    //    ALLOCATION SETUP FOR SUBUNIT 1 AND 2 OF UNIT 2   \\
+    //  *************************************************  \\
+
+    // Click the set button for the subunits of business unit 1 from the org. structure
+    cy.setTotals(company.organizationalStructure.levelTwo_unitTwo.name);
+
+    // Set subunit 1 of business unit 1 allocation for 1st month
+    cy.editAllocationTableCell(1, 1, revenue_only.unit2_subunit1_revenue_lvl2); // row, month, value
+
+    // Check if the value was set for the 1st month
+    cy.checkAllocationCellValue(1, 1, revenue_only.unit2_subunit1_revenue_lvl2); // row, month, value
+
+    // Apply to all remaining months
+    cy.applyToAllFieldsAllocation(1, 1); // row, month
+
+    // Set subunit 1 of business unit 1 allocation for 12th month
+    cy.editAllocationTableCell(2, 1, revenue_only.unit2_subunit2_revenue_lvl2); // row, month, value
+
+    // Check if the value was set for the 12th month
+    cy.checkAllocationCellValue(2, 1, revenue_only.unit2_subunit2_revenue_lvl2); // row, month, value
+
+    // Apply to all remaining months
+    cy.applyToAllFieldsAllocation(2, 1); // row, month
+
+    // Click the set button for the subunits of business unit 1 from the org. structure
+    cy.setTotals(company.organizationalStructure.levelTwo_unitOne.name);
+
+
+    //  *************************************************  \\
+    //               TOTALS RESULT ASSERTION               \\
+    //  *************************************************  \\
+
+
+    // Assert company revenue 12 month value
+    cy.checkTotalCellValue(1, 12, revenue_only.company_12); // row, month, value
+
+    // Assert company revenue 1 year value
+    cy.checkTotalCellValue(1, 13, revenue_only.company_Y1); // row, month, value
+
+    // Assert company revenue 24 month value
+    cy.checkTotalCellValue(1, 24, revenue_only.company_Y2); // row, month, value
+
+    // Assert company level 2 unit 1 revenue 12 month value
+    cy.checkTotalCellValue(2, 12, revenue_only.bu1_12); // row, month, value
+
+    // Assert company level 2 unit 1 revenue 1 year value
+    cy.checkTotalCellValue(2, 13, revenue_only.bu1_Y1); // row, month, value
+
+    // Assert company level 2 unit 1 revenue 24 month value
+    cy.checkTotalCellValue(2, 24, revenue_only.bu1_Y2); // row, month, value
+
+    // Assert company level 2 unit 2 revenue 12 month value
+    cy.checkTotalCellValue(6, 12, revenue_only.bu2_subunit1_12); // row, month, value
+
+    // Assert company level 2 unit 2 revenue 1 year value
+    cy.checkTotalCellValue(6, 13, revenue_only.bu2_subunit1_Y1); // row, month, value
+
+    // Assert company level 2 unit 2 revenue 24 month value
+    cy.checkTotalCellValue(6, 24, revenue_only.bu2_subunit1_Y2); // row, month, value
+
+    // Click the save and close button
+    cy.clickButton('Save & Close');
+
+    // Intercept post revenue fetch
+    cy.intercept('POST', `/api/chart_of_accounts`).as('chartOfAccounts');
+
+    // Assert if the revenue was successfully deleted
+    cy.wait('@chartOfAccounts', { timeout: 100000 })
+      .its('response.statusCode')
+      .should('eq', 200);
+  });
+
 
   it('Should be able to create service revenues', () => {
     // Assert if you are on Forecast revenues section
@@ -413,31 +572,6 @@ describe('Forecast / Revenues Module', () => {
     cy.setUnitPriceInfo(twelve_months_subscription_revenue);
   });
 
-  it('Should be able to create revenue only', () => {
-    // Assert if you are on Forecast revenues section
-    cy.url()
-      .should('eq', 'https://test.hz.modeliks.com/forecast/revenue');
-
-    // Click on Add Revenue Stream
-    cy.addRevenueStream();
-
-    // Populate Revenue Name input field
-    cy.setRevenueName(revenue_only.name);
-
-    // Choose the type of revenue
-    cy.chooseRevenueType(revenue_only);
-
-    // Assert that the actual checkbox is checked after clicking the custom UI
-    cy.assertRevenueType(revenue_only.index);
-
-    // Click next button
-    cy.get('button > span')
-      .contains('Next')
-      .click();
-
-    // Set Unit Price info
-    cy.setUnitPriceInfo(revenue_only);
-  });
 
   // it('Should delete revenues', () => {
 
