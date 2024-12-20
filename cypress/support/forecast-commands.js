c4cc57958181f979838569356b2193f46f1863b0
DaNStanK <daniel.stankovski@gmail.com>
2024-12-20 10:42:56 +0100

    Renamed the create-revenues-commands into forecast-commands file

--- cypress/support/forecast-commands.js
+++ cypress/support/forecast-commands.js
@@ -61,48 +61,6 @@ Cypress.Commands.add('setAllocationMethodology', (methodology) => {
       .should('exist');
 });
 
-
-// Populate Revenue Name input field
-Cypress.Commands.add('setRevenueName', (revenueName) => {
-   // Validate input
-   if (revenueName == null || typeof revenueName !== 'string') {
-      throw new Error('Invalid or missing revenue name. Ensure the revenue name is populated as a parameter in the function and is a string.');
-   }
-
-   // Populate Revenue Name input field
-   cy.get('#revenueNameInput')
-      .clear() // Clear any existing value
-      .type(revenueName); // Type the new revenue name
-});
-
-// Choose the type of revenue
-Cypress.Commands.add('chooseRevenueType', (index) => {
-   // Validate input
-   if (index == null || typeof index !== 'number') {
-      throw new Error('Invalid or missing revenue type. Ensure the revenue type is populated as a parameter in the function and is a number.');
-   }
-
-   // Choose the revenue type based on the index
-   cy.get('div[role="dialog"] label')
-      .eq(index)
-      .find('input[type="checkbox"]')
-      .click({ force: true });
-});
-
-// Assert Revenue type
-Cypress.Commands.add('assertRevenueType', (index) => {
-   // Validate input
-   if (index == null || typeof index !== 'number') {
-      throw new Error('Invalid or missing revenue type. Ensure the revenue type is populated as a parameter in the function and is a number.');
-   }
-
-   // Assert that the checkbox for the revenue type is checked
-   cy.get('div[role="dialog"] label')
-      .eq(index)
-      .find('input[type="checkbox"]')
-      .should('be.checked');
-});
-
 // Click on apply to all fields button 
 Cypress.Commands.add('applyToAllFields', (row, month) => {
    // Validate inputs
