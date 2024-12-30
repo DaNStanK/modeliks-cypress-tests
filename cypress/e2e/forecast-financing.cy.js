31012cbd87d8d8eecbeca3e1b176cd9ff5049738
DaNStanK <daniel.stankovski@gmail.com>
2024-12-30 16:34:41 +0100

    Financing test file and blank tests on all levels created

---
+++ cypress/e2e/forecast-financing.cy.js
@@ -0,0 +1,119 @@
+/// <reference types="cypress" />
+
+describe("Forecast Employees", () => {
+   beforeEach(() => {
+     // Login the user
+     cy.loginUser(Cypress.env('LOGIN_USERNAME'), Cypress.env('LOGIN_PASSWORD'));
+   });
+ 
+    it.only(`Should be able to create blank Financing L1`, () => {
+       // Assert if you are on Forecast revenues section
+       cy.expectedUrl('https://test.hz.modeliks.com/forecast/revenue');
+ 
+       // Click on expenses section
+       cy.clickButton('Financing')
+ 
+       cy.wait(500);
+ 
+       // Click on Add Financing Stream button
+       cy.clickButton('Add Financing');
+ 
+       // Populate expenses name input field
+       cy.setRevenueName('Blank Financing L1');
+
+       // Set interest rate
+       cy.setInterestRate(10)
+ 
+        // Select advance settings
+        cy.chooseAdvanceSettings();
+ 
+        // Choose planning level
+        cy.choosePlanningLevel('Level 1');
+ 
+        // Click save button in the advanced settings
+        cy.clickButton('Save');
+ 
+       // Click next button
+       cy.clickButton('Next');
+        
+       // Click next button
+       cy.clickButton('Next');
+ 
+       // Click save and close button
+       cy.clickButton('Save & Close');
+    });
+ 
+    it.only(`Should be able to create blank Financing L2`, () => {
+       // Assert if you are on Forecast revenues section
+       cy.expectedUrl('https://test.hz.modeliks.com/forecast/revenue');
+ 
+       // Click on expenses section
+       cy.clickButton('Financing')
+ 
+       cy.wait(500);
+ 
+       // Click on Add Financing Stream button
+       cy.clickButton('Add Financing');
+ 
+       // Populate expenses name input field
+       cy.setRevenueName('Blank Financing L2');
+
+       // Set interest rate
+       cy.setInterestRate(10)
+ 
+        // Select advance settings
+        cy.chooseAdvanceSettings();
+ 
+        // Choose planning level
+        cy.choosePlanningLevel('BU');
+ 
+        // Click save button in the advanced settings
+        cy.clickButton('Save');
+ 
+       // Click next button
+       cy.clickButton('Next');
+  
+       // Click next button
+       cy.clickButton('Next');
+ 
+       // Click save and close button
+       cy.clickButton('Save & Close');
+    });
+ 
+    it.only(`Should be able to create blank Financing L3`, () => {
+       // Assert if you are on Forecast revenues section
+       cy.expectedUrl('https://test.hz.modeliks.com/forecast/revenue');
+ 
+       // Click on expenses section
+       cy.clickButton('Financing')
+ 
+       cy.wait(500);
+ 
+       // Click on Add Financing Stream button
+       cy.clickButton('Add Financing');
+ 
+       // Populate expenses name input field
+       cy.setRevenueName('Blank Financing L1');
+
+       // Set interest rate
+       cy.setInterestRate(10)
+ 
+        // Select advance settings
+        cy.chooseAdvanceSettings();
+ 
+        // Choose planning level
+        cy.choosePlanningLevel('SU');
+ 
+        // Click save button in the advanced settings
+        cy.clickButton('Save');
+                
+       // Click next button
+       cy.clickButton('Next');
+ 
+       // Click next button
+       cy.clickButton('Next');
+ 
+       // Click save and close button
+       cy.clickButton('Save & Close');
+    });
+ });
