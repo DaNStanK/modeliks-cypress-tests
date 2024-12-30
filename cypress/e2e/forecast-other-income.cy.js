89409a96211c221201da07855f4f9ad3c25f8093
DaNStanK <daniel.stankovski@gmail.com>
2024-12-30 16:35:48 +0100

    Other incomes test file and blank tests on all levels created

--- cypress/e2e/forecast-other-income.cy.js
+++ cypress/e2e/forecast-other-income.cy.js
@@ -1,12 +1,12 @@
 /// <reference types="cypress" />
 
-describe("Forecast Employees", () => {
+describe("Forecast Other Incomes", () => {
   beforeEach(() => {
     // Login the user
     cy.loginUser(Cypress.env('LOGIN_USERNAME'), Cypress.env('LOGIN_PASSWORD'));
   });
 
-   it.only(`Should be able to create blank Other Income`, () => {
+   it(`Should be able to create blank Other Income L1`, () => {
       // Assert if you are on Forecast revenues section
       cy.expectedUrl('https://test.hz.modeliks.com/forecast/revenue');
 
@@ -19,7 +19,78 @@ describe("Forecast Employees", () => {
       cy.clickButton('Add Other Income');
 
       // Populate expenses name input field
-      cy.setRevenueName('Blank Other Income');
+      cy.setRevenueName('Blank Other Income L1');
+
+       // Select advance settings
+       cy.chooseAdvanceSettings();
+
+       // Choose planning level
+       cy.choosePlanningLevel('Level 1');
+
+       // Click save button in the advanced settings
+       cy.clickButton('Save');
+
+      // Click next button
+      cy.clickButton('Next');
+
+      // Click save and close button
+      cy.clickButton('Save & Close');
+   });
+
+   it(`Should be able to create blank Other Income L2`, () => {
+      // Assert if you are on Forecast revenues section
+      cy.expectedUrl('https://test.hz.modeliks.com/forecast/revenue');
+
+      // Click on expenses section
+      cy.clickButton('Other Income')
+
+      cy.wait(500);
+
+      // Click on Add Other Expenses Stream button
+      cy.clickButton('Add Other Income');
+
+      // Populate expenses name input field
+      cy.setRevenueName('Blank Other Income L2');
+
+       // Select advance settings
+       cy.chooseAdvanceSettings();
+
+       // Choose planning level
+       cy.choosePlanningLevel('BU');
+
+       // Click save button in the advanced settings
+       cy.clickButton('Save');
+
+      // Click next button
+      cy.clickButton('Next');
+
+      // Click save and close button
+      cy.clickButton('Save & Close');
+   });
+
+   it(`Should be able to create blank Other Income L3`, () => {
+      // Assert if you are on Forecast revenues section
+      cy.expectedUrl('https://test.hz.modeliks.com/forecast/revenue');
+
+      // Click on expenses section
+      cy.clickButton('Other Income')
+
+      cy.wait(500);
+
+      // Click on Add Other Expenses Stream button
+      cy.clickButton('Add Other Income');
+
+      // Populate expenses name input field
+      cy.setRevenueName('Blank Other Income L1');
+
+       // Select advance settings
+       cy.chooseAdvanceSettings();
+
+       // Choose planning level
+       cy.choosePlanningLevel('SU');
+
+       // Click save button in the advanced settings
+       cy.clickButton('Save');
 
       // Click next button
       cy.clickButton('Next');
